a602bea438e018766b404194e1a2dad1af9e15f2
Beatriz Ferreira <biatizferreira04@gmail.com>
2024-01-01 19:57:03 +0000

    Best Flight fixes

--- code/DataReader.h
+++ code/DataReader.h
@@ -13,11 +13,8 @@
 #include "FlightGraph.h"
 
 struct pair_hash {
-    template <class T1, class T2>
-    std::size_t operator () (const std::pair<T1, T2> &p) const {
-        auto hash1 = std::hash<T1>{}(p.first);
-        auto hash2 = std::hash<T2>{}(p.second);
-        return hash1 ^ hash2;
+    inline std::size_t operator()(const std::pair<std::string, std::string> &v) const {
+        return std::hash<std::string>()(v.first) ^ std::hash<std::string>()(v.second);
     }
 };
 
